0a7d739b209ee917359d3afbd5a247ef57e4847c
Julie Buser <jbuser2013@gmail.com>
2015-08-28 14:27:36 -0500

    revised header title and tagline

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <div class = "header">
 	<div class = "image">
 		<img src="../img/headerPic.jpg">
-		<h1 class="headerTitle"><span><strong>Vagabond</strong></span></h1><h5 class="headerTagline"><span>Your	 Virtual Travel Getaway</span></h5>
+		<h1 class="headerTitle"><span><strong>VagabondTrip</strong></span></h1><h5 class="headerTagline"><span>Your No Cost Virtual Getaway</span></h5>
 	</div>
 </div>
