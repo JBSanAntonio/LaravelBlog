7aaa48cff3537643df6b5c52f679ca7153aa13f8
Julie Buser <jbuser2013@gmail.com>
2015-08-28 15:22:30 -0500

    consolidated two home.blade.php pages

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,14 +1,32 @@
 @extends('layouts.master')
-<html>
 <head>
 	<title>Home Page</title>
 </head>
-<body>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-66832743-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 @section('content')
 
-	Home Page is Coming
+	<p></p>
+	<h2 class = "intro">
+	Do you long to getaway but can't spare the time or money? 
 
-</body>
-</html>
+	Vagabondtrip.com is your short-term solution. Visit to escape the mundane. View exotic and exciting destinations near and far. 
+
+	No need to struggle with airports and transfers. No need to incur a large balance on your card.  Vagabondtrip takes you where you have been dreaming of going without worry, stress, or cost.
+
+	Relax, browse, and ejoy your virtual trip, or share your own travel experience with others. All you need is a Vagabondtrip Passport to start posting.
+
+	Bon Voyage! 
+	</h2>
 
 @stop
