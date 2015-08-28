e162db68de875cb4f0857ed5a22f3491fa6e3e6d
Julie Buser <jbuser2013@gmail.com>
2015-08-28 14:36:17 -0500

    added description to home page

--- app/views/posts/home.blade.php
+++ app/views/posts/home.blade.php
@@ -17,7 +17,16 @@
 
 @section('content')
 
-	Home Page is Coming
+	<p></p>
+	Do you long to getaway but can't spare the time or money? 
+
+	Vagabondtrip.com is your short-term solution. Visit to escape the mundane. View exotic and exciting destinations near and far. 
+
+	No need to struggle with airports and transfers. No need to incur a large balance on your card.  Vagabondtrip takes you where you have been dreaming of going without worry, stress, or cost.
+
+	Relax, browse, and ejoy your virtual trip, or share your own travel experience with others. All you need is a Vagabondtrip Passport to start posting.
+
+	Bon Voyage! 
 
 </body>
 </html>
