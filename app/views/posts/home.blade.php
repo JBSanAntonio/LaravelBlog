ee0c0cd64bcdcbc5eede83f6ce93f455175465e7
Julie Buser <jbuser2013@gmail.com>
2015-08-28 15:08:20 -0500

    revised home page

--- app/views/posts/home.blade.php
+++ app/views/posts/home.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.master')
-<html>
 <head>
 	<title>Home Page</title>
 </head>
-<body>
-	<script>
+
+<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -30,7 +29,5 @@
 	Bon Voyage! 
 	</h2>
 
-</body>
-</html>
-
 @stop
+
