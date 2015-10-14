2a1d7791491e42626b8ccfc0a352e5510902fd73
Julie Buser <jbuser2013@gmail.com>
2015-10-14 13:02:51 -0500

    changed home page name to launch

--- app/views/partials/resportf-footer.blade.php
+++ app/views/partials/resportf-footer.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="footerText">
 
-      	<a href="{{action("HomeController@showResportfHome") }}">Home | </a></li>
+      	<a href="{{action("HomeController@showLaunch") }}">Home | </a></li>
       	<a href="{{action("HomeController@showResume") }}">The RESUME | </a>
       	<a href="{{action("HomeController@showPortfolioTwo") }}">The PORTFOLIO</a>
      	
